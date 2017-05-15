ada42b473720f16e359175143af61ba6402b58e0
Matthew Lootens <mlootens@gmail.com>
2017-05-15 15:44:07 -0400

    Refactored the code to move the merge subroutine out of the main program.

--- merge_sort.py
+++ merge_sort.py
@@ -1,56 +1,49 @@
-def mergeSort(list):
+def merge(left_list, right_list, inversion_count = 0):
+    sortedlist = []
+    i, j = 0, 0
+    #while i < (len(right_list)) or j < (len(left_list)):
+    while True:
+        if left_list[i] <= right_list[j]:
+            sortedlist.append(left_list[i])
+            if i < len(left_list) - 1:#prevents falling off list
+                i += 1
+            else:#if right_list is empty, append remainder of left_list to sortedlist
+                sortedlist += right_list[j:]
+                break
+        else:
+            sortedlist.append(right_list[j])
+            #inversion_count += len(left_list) - i
+            if j < len(left_list) - 1:
+                j += 1
+            else:
+                sortedlist += left_list[i:]
+                break
+    # if len(sortedlist) == len(standard):
+    #      print "Final Result: " + str(inverse)
+    return sortedlist
+
+def mergeSort(list, inversion_count = 0):
     """list: a list of any n integers, duplicates allowed
     Returns a sorted list in non-decreasing order."""
-    global inverse
-    global standard
     #Handles the base case and empty lists.
-    sortedlist = []
     if len(list) <= 1:
         return list
-        
     else:
-        ###
-        #The recursive call.
-        ###
-        
         x = len(list)
-        lista = mergeSort(list[0:(x//2)])
-        listb = mergeSort(list[(x//2):x+1])
-        
-        ####
-        #The merge sub-routine.
-        ####
-        
-        i, j = 0, 0
-        #while i < (len(lista)) or j < (len(listb)): 
-        while True:
-            if lista[i] <= listb[j]:
-                sortedlist.append(lista[i])
-                if i < (len(lista)-1):#prevents falling off list
-                    i = i + 1
-                else:#if lista is empty, append remainder of listb to sortedlist
-                    sortedlist = sortedlist + listb[j:]
-                    break
-            else:
-                sortedlist.append(listb[j])
-		inverse = inverse + (len(lista) - i)
-                if j < (len(listb)-1):
-                    j = j + 1
-                else:
-                    sortedlist = sortedlist + lista[i:]
-                    break
-        if len(sortedlist) == len(standard):
-	         print "Final Result: " + str(inverse)
-        return sortedlist
+        left_list = mergeSort(list[0:(x // 2)])
+        right_list = mergeSort(list[(x // 2):x + 1])
 
+        return merge(left_list, right_list)
 
-inverse = 0
-file = 'data.txt'
-datafile = open(file)
-datalist = []
-for line in datafile:
-    datalist.append(int(line))
-standard = len(datalist)
+if __name__ == '__main__':
+    #inverse = 0
+    #file = 'inversion_data.txt'
+    # datafile = open(file)
+    # datalist = []
+    # for line in datafile:
+    #     datalist.append(int(line))
+    # standard = len(datalist)
 
-num_list = [6, -3, 213, 4, 403, 1000, 2, 0, 50, -12, 34, 34, 45, 32, -12, 0]
-print(mergeSort(num_list))
+    num_list = [6, -3, 213, 4, 403, 1000, 2, 0, 50, -12, 34, 34, 45, 32, -12, 0]
+    #num_list = [4,3,2,1]
+    print(mergeSort(num_list))
