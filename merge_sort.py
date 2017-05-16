224c897df71c6265fd76f3e143656a3abbe2be1d
Matthew Lootens <mlootens@gmail.com>
2017-05-16 00:03:04 -0400

    A working version of merge sort with an inversion count. Some of the code was also cleaned up.

--- merge_sort.py
+++ merge_sort.py
@@ -1,49 +1,58 @@
-def mergeSort(list, inversion_count = 0):
-    """list: a list of any n integers, duplicates allowed
-    Returns a sorted list in non-decreasing order."""
-    #Handles the base case and empty lists.
+def mergeSort(input_list):
+    """
+    input_list: a list of any n integers, duplicates allowed
+    Returns a tuple:
+        sorted list in non-decreasing order;
+        and a count of inversions.
+    """
+
     def merge(left_list, right_list):
-        nonlocal inversion_count
+        nonlocal inversion_count#Allows for this function to have closure
         sortedlist = []
         i, j = 0, 0
-        #while i < (len(right_list)) or j < (len(left_list)):
-
         while True:
             if left_list[i] <= right_list[j]:
                 sortedlist.append(left_list[i])
                 if i < len(left_list) - 1:#prevents falling off list
                     i += 1
-                else:#if right_list is empty, append remainder of left_list to sortedlist
+                else:#If left is empty, append right directly.
                     sortedlist += right_list[j:]
                     break
             else:
                 sortedlist.append(right_list[j])
-                inversion_count += len(left_list) - i
-                if j < len(left_list) - 1:
+                inversion_count += len(left_list) - i#Count inversions
+                if j < len(right_list) - 1:
                     j += 1
                 else:
                     sortedlist += left_list[i:]
                     break
         return sortedlist
 
-    if len(list) <= 1:
-        return list
+    #####
+    #Basecase and empty list
+    #####
+    if len(input_list) <= 1:
+        inversion_count = 0
+        return input_list, inversion_count
+
+    #####
+    #Recursive calls and book-keeping
+    #####
     else:
-        x = len(list)
-        left_list = mergeSort(list[0:(x // 2)], inversion_count)
-        right_list = mergeSort(list[(x // 2):x + 1], inversion_count)
-        print(inversion_count)
-        return merge(left_list, right_list)
+        left_list, inversion_count = mergeSort(input_list[:len(input_list) // 2])
+        #Need the right_list_tuple variable as a temp holder so that inversion_
+        #count can be incremented.
+        right_list_tuple = mergeSort(input_list[len(input_list) // 2:])
+        right_list = right_list_tuple[0]
+        inversion_count += right_list_tuple[1]
+        sorted_list = merge(left_list, right_list)
+        return sorted_list, inversion_count
 
 if __name__ == '__main__':
-    #inverse = 0
-    #file = 'inversion_data.txt'
-    # datafile = open(file)
-    # datalist = []
-    # for line in datafile:
-    #     datalist.append(int(line))
-    # standard = len(datalist)
-
-    #num_list = [6, -3, 213, 4, 403, 1000, 2, 0, 50, -12, 34, 34, 45, 32, -12, 0]
-    num_list = [4,3,2,1,5, 6, 7]
-    print(mergeSort(num_list))
+    #Load in the data from a file
+    filename = 'inversion_data.txt'
+    with open(filename) as f:
+        datalist = []
+        for line in f:
+            datalist.append(int(line))
+    print(mergeSort(datalist)[1])#Correct answer: 2407905288 (2,407,905,288)
