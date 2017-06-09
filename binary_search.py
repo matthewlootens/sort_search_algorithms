23f8f42dc0ccb39eba9c87b4a8533e447638baf1
Matthew Lootens <mlootens@gmail.com>
2017-06-09 15:17:49 -0400

    binary_search: move test suite into main()

--- binary_search.py
+++ binary_search.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def binarySearch(sorted_list, target):
+def binary_search(sorted_list, target):
     """
     Searches through a list for target.
-    
+
     sorted_list: a non-empty list of non-decreasing ints, floats, or strings
     target: a literal of the same type as elements of sorted_list
     returns: a tuple (False) if the target is not in the list; (True, index_num)
-    if the target is there. 
-    
+    if the target is there.
+
     Duplicates are allowed, but if found, the index of an arbitrary duplicate
     element will be returned.
-    
+
     Raises AssertionError if the passed list is empty.
     """
-    
-    #Initialize variables    
+
+    #Initialize variables
     assert len(sorted_list) > 0
     min = 0
     max = len(sorted_list) - 1
@@ -29,23 +29,30 @@ def binarySearch(sorted_list, target):
             return (False)
         elif sorted_list[guess] > target:
             max = guess - 1
-            guess = (max + min) // 2           
+            guess = (max + min) // 2
         else:
             min = guess + 1
             guess = (max + min) // 2
 
-#Test suite            
-if __name__ == '__main__':
+def main():
+    """
+    Test suite
+    """
     a = [1,2,3,4,5,6,7,9]
     b = []
     c = [5,5,5,5,5]
-    d = [5,5,5,5,5,6,7]   
-    
-    print(binarySearch(a, 5))#(True, 4)
-    print(binarySearch(a, 10))#False
+    d = [5,5,5,5,5,6,7]
+
+    print(binary_search(a, 5))#(True, 4)
+    print(binary_search(a, 10))#False
     try:
-        print(binarySearch(b, 7))
+        print(binary_search(b, 7))
     except AssertionError:
         print("AssertionError raised.")#"AssertionError raised."
-    print(binarySearch(c, 5))#(True, 2)
-    print(binarySearch(d, 5))#(True, 3)
+    print(binary_search(c, 5))#(True, 2)
+    print(binary_search(d, 5))#(True, 3)
+
+
+#Test suite
+if __name__ == '__main__':
+    main()
